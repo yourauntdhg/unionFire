ea19e5e1ac1064a7326e7039eebf4804ee281c2b
qdexp <Pass1234>
2019-09-04 13:41:41 +0800

    以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候取出ApplicaitonContext.

--- unionFire-system/src/main/java/com/unionFire/UnionFireApplication.java
+++ unionFire-system/src/main/java/com/unionFire/UnionFireApplication.java
@@ -1,7 +1,9 @@
 package com.unionFire;
 
+import com.unionFire.utils.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,5 +14,10 @@ public class UnionFireApplication {
 
     public static void main(String[] args){
         SpringApplication.run(UnionFireApplication.class);
+    }
+
+    @Bean
+    public SpringContextHolder springContextHolder(){
+        return new SpringContextHolder();
     }
 }
