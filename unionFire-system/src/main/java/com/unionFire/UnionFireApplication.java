e1b79f718446b2630603f572eb164d8c77542863
qdexp <Pass1234>
2019-09-04 09:29:19 +0800

    初始版本

---
+++ unionFire-system/src/main/java/com/unionFire/UnionFireApplication.java
@@ -0,0 +1,16 @@
+package com.unionFire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableAsync //异步注解，搭配Async使用
+@SpringBootApplication
+@EnableTransactionManagement // 启注解事务管理，等同于xml配置方式的 <tx:annotation-driven />
+public class UnionFireApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(UnionFireApplication.class);
+    }
+}
